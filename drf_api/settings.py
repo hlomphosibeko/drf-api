20ea8c879513cd5281b596afea99bcca1c8fcd7a
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-04-10 13:02:40 +0200

    Install libraries to store the code

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -95,7 +95,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'coresheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
