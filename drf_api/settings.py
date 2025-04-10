c361188cd26aa46e223f771b5cc5e88fdc80dc0d
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-04-10 12:32:06 +0200

    Tidy up code

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['loacalhost', '127.0.0.1', 'drf_api.herokuapp.com']
+ALLOWED_HOSTS = ['loacalhost', '127.0.0.1', 'drf-api.herokuapp.com']
 
 
 # Application definition
