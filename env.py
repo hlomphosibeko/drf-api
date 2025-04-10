3d8c9c78aba61952446672c07801e49ea9163b5f
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-04-10 11:03:15 +0200

    Update the env.py file with secret key

--- env.py
+++ env.py
@@ -5,4 +5,4 @@ os.environ[
     ] = 'cloudinary://683322417993834:FqCJYTpinRPWU_oEgba-HmcTYKE@dibjhrwcc'
 os.environ['DEV'] = '1'
 os.environ['DATABASE_URL'] = "postgresql://neondb_owner:w0pPGHkb4Zsv@ep-lingering-sunset-a2unopg8.eu-central-1.aws.neon.tech/trump_nutty_sadly_54003"
-os.environ.setdefault("SECRET_KEY", "CreateANEWRandomValueHere")
+os.environ.setdefault("SECRET_KEY", "fZVWOjpaVx")
